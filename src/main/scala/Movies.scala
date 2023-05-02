85e5d0d617eb95bd1f0b308117d2a450b8a64899
rocean <rocean74@gmail.com>
2023-05-03 02:15:21 +0300

    Get schema

--- src/main/scala/Movies.scala
+++ src/main/scala/Movies.scala
@@ -3,6 +3,21 @@ import org.apache.spark.sql.functions.{col, explode, first, lower, regexp_replac
 
 object Movies {
     def main(args: Array[String]): Unit = {
+        val ss = SparkSession
+            .builder()
+            .appName("AirTweets")
+            .config("spark.master", "local")
+            .getOrCreate()
 
+        val inputFile = "input/movies.csv"
+
+        // Διάβασμα του αρχείου csv
+        val df = ss.read.option("header", "true").csv(inputFile)
+            // Αφαίρεση σημείων στίξης από τη στήλη text. Μετατροπή όλων των χαρακτήρων σε πεζά.
+            .withColumn("title", regexp_replace(col("title"), "[^A-Za-z0-9]+", " "))
+            .withColumn("title", lower(col("title")))
+
+        df.show()
+        df.printSchema()
     }
 }
