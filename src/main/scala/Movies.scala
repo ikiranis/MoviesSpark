f14cfafde758b81ae7af102e00d0d1e5de10ab04
rocean <rocean74@gmail.com>
2023-05-03 03:05:50 +0300

    Count movies in years

--- src/main/scala/Movies.scala
+++ src/main/scala/Movies.scala
@@ -13,22 +13,28 @@ object Movies {
 
         // Διάβασμα του αρχείου csv και επεξεργασία των απαραίτητων στηλών
         val df = ss.read.option("header", "true").csv(inputFile)
-            // Get year column from title in format (YYYY)
+            // Get year column from title in format (YYYY). If year not found (is number with 4 digits), set to 0
             .withColumn("year", regexp_replace(col("title"), ".*\\((\\d{4})\\).*", "$1"))
+            .filter(col("year").cast("int") > 1900) // Fix bad year values
             // Get title column without year
             .withColumn("title", regexp_replace(col("title"), "\\s*\\(\\d{4}\\)", ""))
-            // Split genres column by | and explode
-            .withColumn("genres", explode(split(col("genres"), "\\|")))
+
 
         // Εμφάνιση των κατηγοριών και μέτρηση των ταινιών που ανήκουν σε κάθε μία
-        df.groupBy("genres")
+        df.withColumn("genres", explode(split(col("genres"), "\\|")))
+            .groupBy("genres")
             .count()
             .orderBy(col("genres").asc)
             .show()
 
+        // Μέτρηση των ταινιών που έχουν βγει ανά έτος. Εμφάνιση των 10 πρώτων
+        df.groupBy("year")
+            .count()
+            .orderBy(col("count").desc)
+            .show(10)
 
 
-//        df.show()
+        df.show()
 //        df.printSchema()
     }
 }
